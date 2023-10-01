ee1cba70ee2540b0a78d38bcb2e3200779d4217e
mariecotije <kkmarushka@gmail.com>
2023-10-01 18:16:37 +0200

    added timer, palygame function, fixed mole popup and remove

--- src/js/index.js
+++ src/js/index.js
@@ -3,34 +3,39 @@ const startBtn = document.querySelector('button');
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const scoreElement = document.querySelector('.score');
+
 //step 3
 let score = 0;
 let timeUp = false;
 //step 4
 const randomTime = (min, max) => {
-    let randTime = Math.random();
+    let randTime = Math.floor((Math.random() * (max - min + 1)) + min);
     return randTime;
 };
 //step 5
 const randomHole = (holes) => {
-    let hole
+    const randH = Math.floor(Math.random() * (holes.length));
+    const hole = holes[randH];
     return hole;
 };
 //step 6
-const peep = (randomTime, randomHole) => {
-    hole.classList.toggle = 'active';
-    hole.setTimeout(() => {
-        hole.classList.toggle = 'active';
-    }, 500);
+const peep = () => {
+    hole = randomHole(holes);
+    hole.classList.toggle('active');
+
+    setTimeout(() => {
+        hide();
+    }, randomTime(3000, 6000));
 };
 //step 7
 const whack = () => {
-    scoreElement.innerText = `${score}++`;
-    
+    score++;
+    scoreElement.innerText = `${score}`;
+
 };
 //step 8
 const hide = () => {
-    hole.classList.remove = 'active';
+    hole.classList.remove('active');
 };
 //step 9
 moles.forEach((mole) => {
@@ -40,7 +45,19 @@ moles.forEach((mole) => {
     });
 });
 //step 10
-startBtn.addEventListener('click',(event) => {
+startBtn.addEventListener('click', (event) => {
     /*reset score and time, start the game, and set the timer.*/
     score = 0;
+    scoreElement.innerText = `${score}`
+    let timeUp = false;
+    let timer = 0;
+    const moleInterval = setInterval(() => {
+        peep();
+        timer++;
+        if (timer == 20) {
+            alert("Time is up! Click START to begin again.")
+            timeUp = true;
+            clearInterval(moleInterval)
+        }
+    }, randomTime(2000, 5000));
 });
